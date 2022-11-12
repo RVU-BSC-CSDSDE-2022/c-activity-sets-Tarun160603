6842799fa186191211f7f071e3b61819dc97f061
Tarun  S <117427500+Tarun160603@users.noreply.github.com>
2022-11-12 04:59:43 +0000

    solved problem 2

---
+++ set01/problem02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b,sum=0;
+  printf("enter the values for a and b \n");
+  scanf("%d %d",&a,&b);
+  sum= a + b;
+  printf("the total value %d",sum);
+  return 0;
+}
